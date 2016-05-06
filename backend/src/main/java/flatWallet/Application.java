30a3cf6edb113e7bda8d15e2b4956b9a4bc1b643
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-06 13:02:37 +0200

    * Moving from .properites to .yml * Application asks for facebook permission

--- backend/src/main/java/flatWallet/Application.java
+++ backend/src/main/java/flatWallet/Application.java
@@ -2,12 +2,14 @@ package flatWallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableOAuth2Sso
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class,args);
