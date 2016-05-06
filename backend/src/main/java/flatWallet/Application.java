291fb249651ec6d55429463456de4c642995129b
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-06 12:51:27 +0200

    * Removing unnecessary dependency

--- backend/src/main/java/flatWallet/Application.java
+++ backend/src/main/java/flatWallet/Application.java
@@ -1,7 +1,5 @@
 package flatWallet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,10 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
-		logger.info("Starting app");
 		SpringApplication.run(Application.class,args);
 	}
 }
