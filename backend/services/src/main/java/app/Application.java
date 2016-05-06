1a55ceb7265b05300ce483b9f5d030d529631f0c
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-06 13:35:48 +0200

    * Remodelling structure

--- backend/services/src/main/java/app/Application.java
+++ backend/services/src/main/java/app/Application.java
@@ -1,4 +1,4 @@
-package flatWallet;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- backend/services/src/main/java/flatWallet/rest/RestSample.java
+++
@@ -1,8 +0,0 @@
-package flatWallet.rest;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RestSample {
-
-}
