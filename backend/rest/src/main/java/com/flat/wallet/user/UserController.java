1d77f3d38a69ed6424fbb1d58f10dd867d20708e
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-08 15:45:03 +0200

    * Adding correct client id

--- backend/rest/src/main/java/com/flat/wallet/user/UserController.java
+++ backend/rest/src/main/java/com/flat/wallet/user/UserController.java
@@ -1,6 +1,9 @@
 package com.flat.wallet.user;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -11,5 +14,10 @@ public class UserController {
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
 		return principal;
+	}
+
+	@RequestMapping(value = "/logged", method = RequestMethod.GET)
+	public Authentication test() {
+		return SecurityContextHolder.getContext().getAuthentication();
 	}
 }
