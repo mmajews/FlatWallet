3dc8db887857cbc8af2984f8c5d7bae6b7ff57ec
Marcin Zareba <marcin.zareba1@gmail.com>
2016-05-08 15:54:53 +0200

    Login template

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -2,10 +2,15 @@
 var app = angular.module('flatWallet',['ui.router', 'ngResource']);
 
 app.config(function ($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/loginState');
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'app/home/home.html'
     })
+
+        .state('loginState', {
+            url: '/loginState',
+            templateUrl: 'app/login/login.html',
+        })
 
 })
