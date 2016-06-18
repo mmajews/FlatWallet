0a9d9d93a0e5ff99ad9e2828f00ddea03cde793f
Marcin Zareba <marcin.zareba1@gmail.com>
2016-06-18 22:10:10 +0200

    Dynamic ShoppingList Displaying added to group view

--- app/src/main/resources/static/app/controllers/GroupController.js
+++ app/src/main/resources/static/app/controllers/GroupController.js
@@ -1,8 +1,30 @@
 app.controller('GroupCtrl', function ($scope, $rootScope, $stateParams, TokenStorage, GroupService) {
+    $scope.newItem = "";
+
 
     GroupService.getGroup($stateParams.groupId, function (data) {
         $scope.currentGroup = data.data;
         $rootScope.currentGroup = data.data;
     }, $scope.failure);
+
+    $scope.successAdding = function (data) {
+        GroupService.getGroup($scope.currentGroup.id, function (data) {
+            $scope.currentGroup = data.data;
+            $rootScope.currentGroup = data.data;
+        }, $scope.failure);
+    };
+
+    $scope.success = function (data) {
+        console.log(data);
+    };
+
+    $scope.failure = function (data) {
+        console.log(data);
+    };
+
+    $scope.addItemToList = function () {
+        GroupService.addItemToShoppingList($scope.currentGroup.id, $scope.newItem, $scope.successAdding, $scope.failure);
+        console.log("added " + $scope.newItem);
+    };
 
 });
