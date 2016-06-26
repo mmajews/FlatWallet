84298b05019bddfaa110ab9ae06ca93c58bc799f
Marcin Majewski <majewskimarcin@outlook.com>
2016-06-26 14:04:25 +0200

    * Fixing test

--- app/src/test/java/com/flat/wallet/rest/DefaultSpringRestTest.java
+++ app/src/test/java/com/flat/wallet/rest/DefaultSpringRestTest.java
@@ -1,16 +1,13 @@
 package com.flat.wallet.rest;
 
 import com.flat.wallet.app.Application;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringApplicationConfiguration(classes = Application.class)
 @TestPropertySource(locations = "classpath:test.properties")
-@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
 public class DefaultSpringRestTest {
--- app/src/test/java/com/flat/wallet/rest/group/GroupIntegrationControllerTest.java
+++ app/src/test/java/com/flat/wallet/rest/group/GroupIntegrationControllerTest.java
@@ -13,12 +13,15 @@ import com.flat.wallet.services.SocialUserService;
 import com.jayway.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class GroupIntegrationControllerTest extends DefaultSpringRestTest {
 
 	@Autowired
